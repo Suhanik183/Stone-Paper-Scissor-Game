f9dbc68e5024644f77f24a0fddddf6ca2e324d4e
Suhani <suhanik183@gmail.com>
2021-11-21 22:58:35 +0530

    p1

---
+++ app.py
@@ -0,0 +1,28 @@
+from datetime import date
+
+
+# import Flask ::: for implementig flask server
+from flask import Flask, render_template,request
+
+
+# create a new web-app whixh is a Flask application while __name__	represents that his is  
+#the file that will reresent the web-app		
+app = Flask(__name__,static_url_path='/static')
+
+
+
+#GLOBAL-variables => check_in, check_out.
+
+
+#/ represent the default page llocalhost/
+@app.route("/")
+#Declarator funtion
+def index():
+
+	return render_template("games.html")
+
+
+
+
+if __name__ == '__main__':
+	app.run()  
